5b15ed5586f55435367a97adf7209bc048fd185a
jesusarmandogr <jesusarmandogr@gmail.com>
2017-06-15 14:03:59 -0500

    Add files via upload

---
+++ Ejercicio00/Ejercicio00/Empleados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    public class Empleados
+    {
+        public Empleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.0m;
+            Edad = 0;
+        }
+
+        //public string Nombre;
+        private string _Nombre;
+        public decimal SueldoDiario;
+        public int Edad;
+
+        // ENCAPSULAMIENTO: Sirve para poder tener una variable privada
+        // _Nombre de la clase, pero que pueda ser accesada públicamente 
+        // mediante la variable pública Nombre.
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public decimal CalculaSalario(int Dias)
+        {
+            return Dias * SueldoDiario;
+        }
+    }
+}
---
+++ Ejercicio00/Ejercicio00/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleados empleado = new Empleados();
+            empleado.Edad = 25;
+            empleado.Nombre = "Jesus Armando";
+            //empleado.SueldoDiario = 255.5m;
+            
+            Console.WriteLine("Introduce el sueldo diario: ");
+            var sueldodiario = Console.ReadLine();
+            empleado.SueldoDiario = Convert.ToDecimal(sueldodiario);
+            decimal total = empleado.CalculaSalario(30);
+
+            Console.WriteLine("El salario mensual del empleado " + empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C") + 
+                " (Sueldo Diario = " + empleado.SueldoDiario.ToString("C") + ")");
+            Console.ReadKey();
+
+
+        }
+    }
+}
